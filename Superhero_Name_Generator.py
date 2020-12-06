3e0ce5d5bd24cda822881c79c138e1bd6e815b09
RafaelRubenstien <73216681+RafaelRubenstien@users.noreply.github.com>
2020-12-06 09:05:32 -0800

    Update Superhero_Name_Generator.py

--- Superhero_Name_Generator.py
+++ Superhero_Name_Generator.py
@@ -1,8 +1,8 @@
 def superhero_name_generator():
     print("Welcome to the superhero name generator:\nTo choose your name follow the instructions below")
-    first_part = input("Please enter the name of the city you grew up in. ")
+    first_part = input("Please enter the name of your power. ")
     second_part = input("Please enter the name of your pet or your favorite mythical creature (Ie. Dragon). ")
-    superhero_name = first_part + second_part
+    superhero_name = first_part + " " + second_part
     return superhero_name
 
 
